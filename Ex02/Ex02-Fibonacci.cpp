f8fce57a5d57e0eee746ef78224957b7ca412f1e
Feathergunner <angohz@ymail.com>
2014-11-04 08:46:09 +0100

    Add Exercise 2.2: Algorithm using linear memory and time in n

--- Ex02/Ex02-Fibonacci.cpp
+++ Ex02/Ex02-Fibonacci.cpp
@@ -12,6 +12,25 @@ int getFibo1(int n){
     return getFibo1(n-1)+getFibo1(n-2);
 }
 
+/*
+ 2) computes the n-th Fibonacci-Number in linear time and linear memory(in n)
+*/
+int getFibo2(int n){
+    // trivial cases:
+    if (n<0) return -1;
+    if (n==0) return 0;
+    if (n==1) return 1;
+    //initialize a vector which keeps the i-th fibonacci-number at position i:
+    vector<int> fib_numbers = vector<int>(n);
+    fib_numbers[0]=0;
+    fib_numbers[1]=1;
+    //compute fibonacci-numbers iteratively:
+    for (int i=2;i<n;i++){
+        fib_numbers[i] = fib_numbers[i-1]+fib_numbers[i-2];
+    }
+    return fib_numbers[n-2]+fib_numbers[n-1];
+}
+
 int main(){
 	return 0;
 }
